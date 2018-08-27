624d97c28ff4de70d70a7019ea504e2d3c30a8d5
Mark Miller <725mrm@gmail.com>
2018-08-27 16:31:28 -0700

    oops, turns out 'hardware.vive' doesn't work.

--- remap_controllers.js
+++ remap_controllers.js
@@ -2,18 +2,21 @@ var mappingJSON = {
     "name": "com.highfidelity.controllers.example.jsonMapping",
     "channels": [
         // include this to test whether it's loaded.
-        { "from": "Hardware.Vive.LT", "to": "Actions.TranslateY" },
+        { "from": "Standard.LT", "to": "Actions.TranslateY" },
         { "from": "Standard.RT", "to": "Actions.TranslateY" },
         
         // disable left and right big buttons, to stop rotate and teleport.
-        { "from": "Hardware.Vive.LS", "to": "Actions.Roll" }, // note that roll doesn't do anything, thankfully.
-        { "from": "Hardware.Vive.RS", "to": "Actions.Roll"}
+        { "from": "Standard.LS", "to": "Actions.Roll" }, // note that roll doesn't do anything, thankfully.
+        //{ "from": "Standard.RS", "to": "Actions.Roll"},
+        { "from": "Vive.RS", "to": "Actions.Roll"}
     ]
 };
 
-var mapping = Controller.parseMapping(JSON.stringify(mappingJSON));
-mapping.enable();
+var oldMapping = Controller.
+
+var newMapping = Controller.parseMapping(JSON.stringify(mappingJSON));
+newMapping.enable();
 
 Script.scriptEnding.connect(function () {
-    mapping.disable();
+    newMapping.disable();
 });
