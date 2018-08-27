f97a7ef19095a4da85b8ad88e8c881a98cd32aa9
Mark Miller <725mrm@gmail.com>
2018-08-27 15:56:36 -0700

    oh wait capture and release might work.

--- remap_controllers.js
+++ remap_controllers.js
@@ -1,9 +1,28 @@
-var MAPPING_NAME = "com.highfidelity.controllers.example.newMapping";
-var mapping = Controller.newMapping(MAPPING_NAME);
+/*
+var mappingJSON = {
+    "name": "com.highfidelity.controllers.example.jsonMapping",
+    "channels": [
+        // include this to test whether it's loaded.
+        { "from": "Hardware.Vive.LT", "to": "Actions.TranslateY" },
+        { "from": "Standard.RT", "to": "Actions.TranslateY" },
+        
+        // disable left and right big buttons, to stop rotate and 
+        { "from": "Hardware.Vive.LS", "to": "Actions.Roll" }, // note that roll doesn't do anything, thankfully.
+        { "from": "Hardware.Vive.LS", "to": "Actions.Roll"}, // disable rotate
+    ]
+};
 
-mapping.from(Controller.Standard.RT).to(Controller.Actions.TranslateY);
-Controller.enableMapping(MAPPING_NAME);
+var mapping = Controller.parseMapping(JSON.stringify(mappingJSON));
+mapping.enable();
 
 Script.scriptEnding.connect(function () {
-   Controller.disableMapping(MAPPING_NAME);
-});
+    mapping.disable();
+});*/
+
+Script.setTimeout(function () {
+   Controller.captureActionEvents();
+}, 5000);
+
+Script.setTimeout(function () {
+   Controller.releaseActionEvents();
+}, 10000);
