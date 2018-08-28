f55715cd04146b87eb2510b63739a74a7ae634f5
Mark Miller <725mrm@gmail.com>
2018-08-27 17:05:42 -0700

    abandon mapping, do other stuff.

--- remap_controllers.js
+++ remap_controllers.js
@@ -1,25 +1,7 @@
-var mappingJSON = {
-    "name": "com.highfidelity.controllers.example.jsonMapping",
-    "channels": [
-        // include this to test whether it's loaded.
-        { "from": "Standard.LT", "to": "Actions.TranslateY" },
-        { "from": "Vive.RT", "to": "Actions.TranslateY" },
-        
-        // disable left and right big buttons, to stop rotate and teleport.
-        { "from": "Standard.LS", "to": "Actions.Roll" }, // note that roll doesn't do anything, thankfully.
-        //{ "from": "Standard.RS", "to": "Actions.Roll"},
-        { "from": "Vive.RS", "to": "Actions.Roll"},
-        { "from": "Vive.RS_CENTER", "to": "Actions.Roll"},
-        { "from": "Vive.RS_X", "to": "Actions.Roll"},
-        { "from": "Vive.RS_Y", "to": "Actions.Roll"},
-    ]
-};
-
-var newMapping = Controller.parseMapping(JSON.stringify(mappingJSON));
-newMapping.enable();
-Messages.sendLocalMessage('Hifi-Teleport-Disabler','both');
+Controller.captureActionEvents(); // disable rotation
+Messages.sendLocalMessage('Hifi-Teleport-Disabler','both'); // disable teleporting
 
 Script.scriptEnding.connect(function () {
-    newMapping.disable();
-    Messages.sendLocalMessage('Hifi-Teleport-Disabler','none');
+    Messages.sendLocalMessage('Hifi-Teleport-Disabler','none'); // enable teleporting
+    Controller.releaseActionEvents(); // enable rotation
 });
