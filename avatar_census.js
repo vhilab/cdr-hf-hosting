1c922cb3720b10b7c5a21efcf93f315e3e89ef27
Mark Miller <725mrm@gmail.com>
2019-03-19 13:42:54 -0700

    Update avatar_census.js

--- avatar_census.js
+++ avatar_census.js
@@ -3,8 +3,25 @@ var SEARCH_RADIUS = 1000;
 
 var interval = 1000 * 60 * 5; // every 5 minutes
 
-var send_avatar_ids = function(deltaTime) {
+var send_avatar_ids = function() {
+    print("Requesting IDs")
     var avatar_list = AvatarList.getAvatarIdentifiers();
+    
+    avatar_list.forEach(function(elem) {
+        Users.requestUsernameFromID(elem);
+    });
 };
 
-Script.setInterval(send_avatar_ids, interval);
+function username_reply(nodeID, userName, machineFingerprint, isAdmin) {
+    print("nodeID " + nodeID)
+    print("userName " + userName)
+    print("machineFingerprint " + machineFingerprint)
+    print("isAdmin " + isAdmin)
+}
+
+Users.usernameFromIDReply.connect(username_reply);
+
+//Script.setInterval(send_avatar_ids, interval);
+
+send_avatar_ids()
+
